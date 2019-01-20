f22ffedade1e4e4275e3aae0b9e766a628851849
Unknown <viktoria.kirchleitner@web.de>
2019-01-20 20:02:02 +0100

    Das Projekt
    MainMenu fehlt noch, aber die Beschleunigung mit Touch funtioniert

---
+++ IMGe/Assets/Scripts/ButtonTouch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonTouch : MonoBehaviour, IPointerDownHandler{
+
+    private Image button;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		var gO= GameObject.Find("Button");
+        button = gO.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+    public void Accelerate()
+    {
+
+    }
+
+    public void SlowDown()
+    {
+
+    }
+}
---
+++ IMGe/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarBehaviour : MonoBehaviour {
+
+    Rigidbody rb;
+    public float acceleration;
+    public float maxSpeed;
+   // public int basicZ;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Debug.Log("rb vel in Update" + rb.velocity);
+        
+        
+    }
+
+    public void Accelerate()
+    {
+        // if (Mathf.Abs(rb.velocity.z) < maxSpeed)
+        // {
+        //     rb.velocity = rb.velocity + new Vector3(0, 0, acceleration * Time.deltaTime);
+        // }
+        // rb.velocity = rb.velocity + new Vector3(0, 0, acceleration * Time.deltaTime);
+        //transform.Translate(Vector3.back * Time.deltaTime * acceleration
+        //     );
+        rb.velocity = new Vector3(0, 0, -acceleration);
+        Debug.Log("rb vel:" + rb.velocity);
+    }
+
+    public void SlowDown()
+    {
+        rb.velocity = Vector3.zero;
+    }
+}
