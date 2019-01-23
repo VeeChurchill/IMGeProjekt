db1f8f06361059fa5888f6fe13c5d099c49614f2
hl <hel.lud@gmx.de>
2019-01-23 17:11:12 +0100

    Player movement should work now

---
+++ IMGe/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+	//private static float accelerometerUpdateInterval = 1.0f / 60.0f;
+	private float maxPosX;
+
+	void Start () {
+		maxPosX = 4.5f;
+	}
+
+	void Update () {
+		float gradX = Input.acceleration.y;
+		float newX = transform.position.x + 40 * gradX * Time.deltaTime;
+		newX = newX > maxPosX ? maxPosX : newX;
+		newX = newX < -maxPosX ? -maxPosX : newX;
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+	}
+}
